80c1df1e0e7f859edcaeff29507bcf8c612fcaee
graeme.miller <graeme.miller@cloudsoftcorp.com>
2017-11-19 16:20:05 +0000

    Add scale to y axis

--- create_graph.R
+++ create_graph.R
@@ -6,15 +6,15 @@ a<-read.csv(args[1], sep=",",head=TRUE)
 
 ## Set the output file name/type
 pdf(file="output.pdf")
-## Plot your data
-plot(a$tick,a$moveSpeedPercentage,ylab="moveSpeedPercentage",xlab="tick")
-plot(a$tick,a$maturityAgePercentage,ylab="maturityAgePercentage",xlab="tick")
-plot(a$tick,a$gestationSpeedPercentage,ylab="gestationSpeedPercentage",xlab="tick")
-plot(a$tick,a$litterSizePercentage,ylab="litterSizePercentage",xlab="tick")
-plot(a$tick,a$maxAgePercentage,ylab="maxAgePercentage",xlab="tick")
-plot(a$tick,a$metabolismPercentage,ylab="metabolismPercentage",xlab="tick")
-plot(a$tick,a$hungerLimitToEatPercentage,ylab="hungerLimitToEatPercentage",xlab="tick")
 
+## Plot your data
+plot(a$tick,a$moveSpeedPercentage,ylab="moveSpeedPercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$maturityAgePercentage,ylab="maturityAgePercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$gestationSpeedPercentage,ylab="gestationSpeedPercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$litterSizePercentage,ylab="litterSizePercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$maxAgePercentage,ylab="maxAgePercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$metabolismPercentage,ylab="metabolismPercentage",xlab="tick", ylim=c(0,100))
+plot(a$tick,a$hungerLimitToEatPercentage,ylab="hungerLimitToEatPercentage",xlab="tick", ylim=c(0,100))
 
 ## Close the graphics device (write to the output file)
 dev.off()
